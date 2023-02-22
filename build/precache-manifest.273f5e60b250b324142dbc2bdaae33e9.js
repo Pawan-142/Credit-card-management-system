f145476fbd9b733246e481e0a4b7a4b37d9286a3
Junaid0137 <110022476+Junaid0137@users.noreply.github.com>
2023-02-22 09:42:17 +0530

    Add files via upload

---
+++ build/precache-manifest.273f5e60b250b324142dbc2bdaae33e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d572887640ff09d1846efc7e6d906f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d92a379b016f705d0542",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6fef44294bb467d74c2c",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d92a379b016f705d0542",
+    "url": "/static/js/2.7f38287d.chunk.js"
+  },
+  {
+    "revision": "6fef44294bb467d74c2c",
+    "url": "/static/js/main.d804bf11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
